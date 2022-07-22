8b06ba0bb56a9b1e45709306b27ca2bee0f242b7
joao.moreira1 <joao.moreira1@gitlab.com>
2022-07-21 21:43:24 -0300

    first commit

---
+++ api/src/graphql/posts.sdl.ts
@@ -0,0 +1,31 @@
+export const schema = gql`
+  type Post {
+    id: Int!
+    title: String!
+    body: String!
+    createdAt: DateTime!
+  }
+
+  type Query {
+    posts: [Post!]! @skipAuth
+    post(id: Int!): Post @skipAuth
+  }
+
+  input CreatePostInput {
+    title: String!
+    body: String!
+    createdAt: DateTime!
+  }
+
+  input UpdatePostInput {
+    title: String
+    body: String
+    createdAt: DateTime
+  }
+
+  type Mutation {
+    createPost(input: CreatePostInput!): Post! @requireAuth
+    updatePost(id: Int!, input: UpdatePostInput!): Post! @requireAuth
+    deletePost(id: Int!): Post! @requireAuth
+  }
+`
---
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -0,0 +1,99 @@
+import { FieldError, Form, Label, Submit, TextAreaField, TextField, useForm } from '@redwoodjs/forms'
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags, useMutation } from '@redwoodjs/web'
+
+import { Toaster, toast } from '@redwoodjs/web/toast'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
+const ContactPage = () => {
+  const formMethods = useForm();
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for your message!');
+      formMethods.reset();
+    }
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    create({
+      variables: {
+        input: data
+      }
+    })
+  }
+
+  return (
+    <>
+      <MetaTags title="Contact" description="Contact page" />
+
+      <Toaster />
+      <Form onSubmit={onSubmit} formMethods={formMethods} error={error}>
+        <Label
+          name="name"
+          className="block text-gray-700 uppercase text-sm"
+          errorClassName="block mt-8 text-red-700 uppercase text-sm"
+        >
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          className="border rounded-sm px-2 py-1 outline-none"
+          errorClassName="border rounded-sm px-2 py-1 border-red-700 outline-none"
+        />
+        <FieldError name="name" className="block text-red-700" />
+
+        <Label
+          name="email"
+          className="block mt-8 text-gray-700 uppercase text-sm"
+          errorClassName="block mt-8 text-red-700 uppercase text-sm"
+        >
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+
+          }}
+          className="border rounded-sm px-2 py-1"
+          errorClassName="border rounded-sm px-2 py-1 border-red-700 outline-none"
+        />
+        <FieldError name="email" className="block text-red-700" />
+
+        <Label
+          name="message"
+          className="block mt-8 text-gray-700 uppercase text-sm"
+          errorClassName="block mt-8 text-red-700 uppercase text-sm"
+        >
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          className="block border rounded-sm px-2 py-1"
+          errorClassName="block border rounded-sm px-2 py-1 border-red-700 outline-none"
+        />
+        <FieldError name="message" className="block text-red-700" />
+
+        <Submit
+          className="block bg-blue-700 text-white mt-8 px-4 py-2 rounded"
+          disabled={loading}
+        >
+          Save
+        </Submit>
+
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
